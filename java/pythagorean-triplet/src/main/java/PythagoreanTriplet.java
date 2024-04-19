157b6cc6c799e5f42d5b97967126074788d70667
Aks32216 <amishsingh568@gmail.com>
2024-04-20 00:11:54 +0530

    made changes to pythagorean triplet file

--- java/pythagorean-triplet/src/main/java/PythagoreanTriplet.java
+++ java/pythagorean-triplet/src/main/java/PythagoreanTriplet.java
@@ -1,29 +1,67 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 class PythagoreanTriplet {
 
+    private final int a;
+    private final int b;
+    private final int c;
     PythagoreanTriplet(int a, int b, int c) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.a = a;
+        this.b = b;
+        this.c = c;
     }
-
     static TripletListBuilder makeTripletsList() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return new TripletListBuilder();
     }
-
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PythagoreanTriplet that = (PythagoreanTriplet) o;
+        if (a != that.a) return false;
+        if (b != that.b) return false;
+        return c == that.c;
+    }
+    @Override
+    public int hashCode() {
+        int result = a;
+        result = 31 * result + b;
+        result = 31 * result + c;
+        return result;
+    }
     static class TripletListBuilder {
-
+        private int n;
+        private int maxFactor = 0;
         TripletListBuilder thatSumTo(int sum) {
-            throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+            this.n = sum;
+            return this;
         }
-
         TripletListBuilder withFactorsLessThanOrEqualTo(int maxFactor) {
-            throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+            if (maxFactor > 0) this.maxFactor = maxFactor;
+            return this;
         }
-
         List<PythagoreanTriplet> build() {
-            throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+            List<PythagoreanTriplet> triplets = new ArrayList<>();
+            int N = this.n;
+            if (this.maxFactor > 0) {
+                N = this.maxFactor;
+            }
+            IntStream.range(1, this.n).forEachOrdered(a -> {
+                IntStream.range(a, this.n - a).forEachOrdered(b -> {
+                    int c = this.n - a - b;
+                    if (a*a + b*b == c*c) {
+                        if (maxFactor > 0 && c <= this.maxFactor) {
+                            triplets.add(new PythagoreanTriplet(a,b,c));
+                        } else if (maxFactor <= 0) {
+                            triplets.add(new PythagoreanTriplet(a,b,c));
+                        }
+                    }
+                });
+            });
+            return triplets;
         }
-
     }
 
 }
