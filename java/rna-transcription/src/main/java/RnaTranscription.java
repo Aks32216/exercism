2cb9daa6d7e7d3d366129b4833aafa8eb7591505
Aks32216 <amishsingh568@gmail.com>
2024-04-19 23:50:39 +0530

    done rna transcription

--- java/rna-transcription/src/main/java/RnaTranscription.java
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,25 @@
 class RnaTranscription {
 
+    char getRnaStrandFromDnaStrand(char curDnaStrand){
+        switch (curDnaStrand){
+            case 'G' : return 'C';
+            case 'C' : return 'G';
+            case 'T' : return 'A';
+            case 'A' : return 'U';
+        }
+        return 'A';
+    }
+
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if(dnaStrand.equals(""))
+            return "";
+        StringBuilder rnaStrand=new StringBuilder();
+        for(int i=0;i<dnaStrand.length();++i){
+            char curDnaStrand=dnaStrand.charAt(i);
+            char equivalentRnaStrand=getRnaStrandFromDnaStrand(curDnaStrand);
+            rnaStrand.append(equivalentRnaStrand);
+        }
+        return rnaStrand.toString();
     }
 
 }
